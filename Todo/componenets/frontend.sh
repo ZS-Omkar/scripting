11d5ba1afd50ca28c4745f365af1d4a794d94284
Omkar <omkar.betha@gmail.com>
2021-05-06 22:30:07 +0530

    added

--- Todo/componenets/frontend.sh
+++ Todo/componenets/frontend.sh
@@ -4,36 +4,36 @@ source common.sh
 
 OS_PREREQ
 
-HEAD "Installing Nginx"
+HEADING "Installing Nginx"
 apt install nginx -y &>>"$LOG"
 Stat $?
 
-HEAD "Installing nodejs and npm"
+HEADING "Installing nodejs and npm"
 apt install nodejs -y &>>"$LOG"
 Stat $?
 
 apt install npm -y &>>"$LOG"
 Stat $?
 
-HEAD "changing the directory and creating a new directory"
+HEADING "changing the directory and creating a new directory"
 cd /var/www/html && mkdir vue
 
 cd vue || exit
 
-HEAD "cloning the repo"
+HEADING "cloning the repo"
 git clone  https://github.com/zelar-soft-todoapp/frontend.git
 
 cd frontend || exit
 
-HEAD "Installing the dependencies and run npm"
+HEADING "Installing the dependencies and run npm"
 npm install
 npm run build
 Stat $?
 
-HEAD "Change the path of Nginx"
+HEADING "Change the path of Nginx"
 sed -i -e 's+/var/www/html+/var/www/html/vue/frontend/dist+g' /etc/nginx/sites-available/default
 
-HEAD "Restarting nginx services"
+HEADING "Restarting nginx services"
 systemclt restart nginx
 systemctl enable nginx
 
