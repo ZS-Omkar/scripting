0833af5cc8e944e3d07bd38df81bb02326d21592
Omkar <omkar.betha@gmail.com>
2021-05-07 10:56:57 +0530

    updated

--- Todo/components/frontend.sh
+++ Todo/components/frontend.sh
@@ -5,11 +5,11 @@ source components/common.sh
 OS_PREREQ
 
 HEAD "Installing Nginx"
-apt install nginx -y &>$LOG
+apt install nginx -y &>>$LOG
 
 HEAD "Installing nodejs and npm"
-apt install nodejs -y &>$LOG
-apt install npm -y &>$LOG
+apt install nodejs -y &>>$LOG
+apt install npm -y &>>$LOG
 
 HEAD "changing the directory and creating a new directory"
 cd /var/www/html && mkdir vue
@@ -22,8 +22,8 @@ git clone 'https://github.com/zelar-soft-todoapp/frontend.git'
 cd frontend
 
 HEAD "Installing the dependencies and run npm"
-npm install &>$LOG
-npm run build &>LOG
+npm install &>>$LOG
+npm run build &>>LOG
 
 HEAD "Change the path of Nginx"
 sed -i -e 's+root /var/www/html+root /var/www/html/vue/frontend/dist+g' /etc/nginx/sites-available/default
