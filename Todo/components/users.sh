287dd54364742019c54fa181cf5083e04ee53687
Omkar <omkar.betha@gmail.com>
2021-05-07 13:41:06 +0530

    updated

--- Todo/components/users.sh
+++ Todo/components/users.sh
@@ -21,7 +21,7 @@ Head "cleaning the maven package"
 mvn clean package &>>$LOG
 
 Head "Now move the user services"
-mv /home/todo/user/systemd.service /etc/systemd/system/user.service
+mv /home/todo/user/systemd.service /etc/systemd/system/users.service
 
 Head "Restart the services"
 systemctl daemon-reload && systemctl restart users && systemctl status users
