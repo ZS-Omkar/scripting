a2b78328db6c66d730b429cd0c9299ee84793035
Omkar <omkar.betha@gmail.com>
2021-05-07 12:18:06 +0530

    updated

---
+++ Todo/components/users.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source components/common.sh
+
+OS_PREREQ
+
+Head "Install java-openjdk"
+apt-get install openjdk-8-jdk-headless -y &>>$LOG
+Stat $?
+
+Head "Installing Maven"
+apt install maven -y &>>$LOG
+Stat $?
+
+Head "Cloning the repo"
+git clone https://github.com/ZS-Omkar/users.git
+
+Head "cleaning the maven package"
+mvn clean package
+
+cd users/target
+
+Head "Start the users api services"
+java -jar users-api-0.0.1.jar
