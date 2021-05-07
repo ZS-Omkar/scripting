be09fc64edf5bd11b6e1ed64a89ec98421489cda
Omkar <omkar.betha@gmail.com>
2021-05-07 15:38:02 +0530

    updated

--- Todo/components/login.sh
+++ Todo/components/login.sh
@@ -8,6 +8,7 @@ Head "Installing go from the below link and export the path"
 wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
 export PATH=$PATH:/usr/local/go/bin
 source ~/.profile
+go version
 
 Head "create a directory and cd into the directory and create one more directory"
 mkdir /go
