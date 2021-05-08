a3ef0a99a5102423eed0305c9d3d383857496862
Omkar <omkar.betha@gmail.com>
2021-05-08 17:04:05 +0530

    updated

--- Todo/components/redis.sh
+++ Todo/components/redis.sh
@@ -6,10 +6,12 @@ OS_PREREQ
 
 Head "Install redis-server"
 apt install redis-server -y &>>$LOG
+Stat $?
 
 Head "Changing the IP address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
+Stat $?
 
 Head "Restart the services"
 systemctl start redis
-systemctl status redis
+Stat $?
