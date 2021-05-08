6385637aba8e8352ee14d96a22721a90222c1e9b
Omkar <omkar.betha@gmail.com>
2021-05-08 17:24:51 +0530

    updated

--- Todo/components/redis.sh
+++ Todo/components/redis.sh
@@ -9,7 +9,7 @@ apt install redis-server -y &>>$LOG
 Stat $?
 
 Head "Changing the IP address"
-sed -i -e 's/100.25.141.67/0.0.0.0/' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
 Stat $?
 
 Head "Restart the services"
