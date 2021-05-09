afeca7da5de57432387852e1c6ecff9bb95e9fac
Omkar <omkar.betha@gmail.com>
2021-05-09 16:54:58 +0530

    updated

--- Todo/components/todo.sh
+++ Todo/components/todo.sh
@@ -21,5 +21,6 @@ Head "Changing the path"
 mv /root/scripting/Todo/todo/todo.service /etc/systemd/system/todo.service
 
 Head "Restart the todo service"
+systemctl daemon-reload
 systemctl start todo
 systemctl status todo
